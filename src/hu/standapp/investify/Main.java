62f77e49abfd9dee29ce8831c4b835d21f34d204
mankamolnar <markmanomolnar@gmail.com>
2017-06-20 14:30:57 +0200

    runnable initial version without any content

---
+++ src/hu/standapp/investify/Main.java
@@ -0,0 +1,10 @@
+package hu.standapp.investify;
+
+/**
+ * Created by flowerpower on 2017. 06. 20..
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("kdsaflkaj");
+    }
+}
