43750b5b44624467f9dae8063204c80fa146a282
mankamolnar <markmanomolnar@gmail.com>
2017-06-22 16:30:13 +0200

    junit5 imported and created an empty but working test for House

---
+++ src/test/hu/standapp/investify/model/HouseTest.java
@@ -0,0 +1,54 @@
+package hu.standapp.investify.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by flowerpower on 2017. 06. 22..
+ */
+class HouseTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getArea() {
+    }
+
+    @Test
+    void setArea() {
+    }
+
+    @Test
+    void getZipCode() {
+    }
+
+    @Test
+    void setZipCode() {
+    }
+
+    @Test
+    void getCity() {
+    }
+
+    @Test
+    void setCity() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void setAddress() {
+    }
+
+    @Test
+    void getPredictedIncome() {
+    }
+
+    @Test
+    void setPredictedIncome() {
+    }
+
+}
