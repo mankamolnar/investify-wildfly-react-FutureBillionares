36a54ebc4b3e91d39edb8a644ec228c2a5b27ede
mankamolnar <markmanomolnar@gmail.com>
2017-06-22 17:31:35 +0200

    HousePictureTest, HouseTest, InvestmentTest developed

--- src/test/hu/standapp/investify/model/HouseTest.java
+++ src/test/hu/standapp/investify/model/HouseTest.java
@@ -3,52 +3,68 @@ package hu.standapp.investify.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Created by flowerpower on 2017. 06. 22..
  */
 class HouseTest {
-    @BeforeEach
-    void setUp() {
-    }
+
+    House testHouse = new House(12, 2016, "SzentFcknEndre", "Imádomavelőt utca 12", 500);
 
     @Test
     void getArea() {
+        assertEquals(testHouse.getArea(), 12);
     }
 
     @Test
     void setArea() {
+        testHouse.setArea(13);
+        assertEquals(testHouse.getArea(), 13);
     }
 
     @Test
     void getZipCode() {
+        assertEquals(testHouse.getZipCode(), 2016);
     }
 
     @Test
     void setZipCode() {
+        testHouse.setZipCode(2017);
+        assertEquals(testHouse.getZipCode(), 2017);
     }
 
     @Test
     void getCity() {
+        assertEquals(testHouse.getCity(), "SzentFcknEndre");
     }
 
     @Test
     void setCity() {
+        testHouse.setCity("Budapest");
+        assertEquals(testHouse.getCity(), "Budapest");
     }
 
     @Test
     void getAddress() {
+        assertEquals(testHouse.getAddress(), "Imádomavelőt utca 12");
     }
 
     @Test
     void setAddress() {
+        testHouse.setAddress("fecske u");
+        assertEquals(testHouse.getAddress(), "fecske u");
     }
 
     @Test
     void getPredictedIncome() {
+        assertEquals(testHouse.getPredictedIncome(), 500);
     }
 
     @Test
     void setPredictedIncome() {
+        testHouse.setPredictedIncome(600);
+        assertEquals(testHouse.getPredictedIncome(), 600);
     }
 
 }
