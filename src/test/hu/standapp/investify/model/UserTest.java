ec168d7c6b67d5ea4992f58e4afb5c66463ede29
Mátyás Krista <nincs.matyas@gmail.com>
2017-07-03 09:02:43 +0200

    Changi

--- src/test/hu/standapp/investify/model/UserTest.java
+++ src/test/hu/standapp/investify/model/UserTest.java
@@ -6,13 +6,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
