1076fe1c33bb883b2c73943d4285311af8b2cb42
mankamolnar <markmanomolnar@gmail.com>
2017-07-06 13:32:53 +0200

    finished merging and fixed some bugs

--- src/resources/react/controllers/Faq.jsx
+++ src/resources/react/controllers/Faq.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import {Link} from 'react-router';
 import NavBar from '../components/NavBar';
 import FaqPage from "../components/FaqPage";
-// *** PAGES ***
 
 export default class Faq extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {loggedIn:this.props.loggedIn};
     }
 
     render() {
         return (
             <div>
-                <NavBar loggedIn={this.state.loggedIn} changeState={this.changeState} />
+                <NavBar loggedIn={this.state.loggedIn} />
                 <FaqPage/>
             </div>
         );
